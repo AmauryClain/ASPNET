88e2763d7902ca20aabdbd4e6b5ef8c07fc5ade1
ACLAIN <amaury.clain@viacesi.fr>
2024-08-27 08:53:26 +0200

    Ajoutez des fichiers projet.

---
+++ ASPNET/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using ASPNET.Data;
+using ASPNET.Models.ViewModels;
+using ASPNET.Models.Entities;
+
+
+namespace ASPNET.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(AddStudentViewModel addStudentViewModel)
+        {
+            var student = new Student
+            {
+                Name = addStudentViewModel.Name,
+                Email = addStudentViewModel.Email,
+                Phone = addStudentViewModel.Phone,
+                Subscribed = addStudentViewModel.Subscribed,
+            };
+
+            await dbContext.Students.AddAsync(student);
+
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+		}
+    }
+}
---
+++ ASPNET/Models/ViewModels/AddStudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPNET.Models.ViewModels
+{
+    public class AddStudentViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
